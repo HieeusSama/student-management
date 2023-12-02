62ed0dad958f9a7fd199f867a59ed8a518f7f901
HieeusSama <vhieu31072004@gmail.com>
2023-12-02 10:05:39 +0700

    them thu vien

--- main.cpp
+++ main.cpp
@@ -1,77 +1,10 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "student.h"
 
 using namespace std;
-
-class sinhvien
-{
-    private:
-        char username[10], username1[10];
-        int password, password1;
-        
-    public:
-        void reg()
-        {
-            cout<<"-------Register-------"<<endl;
-            cout<<"Username: "; cin>>username;
-            cout<<"Password: "; cin>>password;
-            cout<<"----------------------";
-            // Ki?m tra tr�ng t�n t�i kho?n ? co so d? li?u
-        }
-        void login()
-        {
-            do
-            {
-                cout<<"-------Login-------"<<endl;
-                cout<<"Username: "; cin>>username1;
-                cout<<"Password: "; cin>>password1;
-            }while(username1 != username and password1 != password);
-        }
-};
-
-class teacher
-{
-    public:
-        vector<sinhvien> teacher;
-};
-
-
-
-void filter(sinhvien sv)
-{
-    int chon, chonlr;
-    cout<<"Ban la ..."<<endl;
-    cout<<"           SinhVien(1)       GiangVien(2)"<<endl;
-    cout<<"->"; cin>>chon;
-    switch (chon)
-    {
-    case 1:
-        cout<<"(1)Login"<<endl;
-        cout<<"(2)Register"<<endl;
-        cin>>chonlr;
-        switch (chonlr)
-        {
-            case 1:
-                sv.login();
-                break;
-            case 2:
-                sv.reg();
-                break;
-            default:
-                cout<<"Vui long ban nhap lai"<<endl;
-                break;
-        }
-    case 2:
-        break;
-    default:
-        cout<<"Vui long ban nhap lai"<<endl;
-        break;
-    }
-}
-
 int main()
 {
-    sinhvien sv;
-    filter(sv);
+    
 }
