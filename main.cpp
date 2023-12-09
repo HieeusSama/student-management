db28e5b9e1739eeb34e40ce4fceec7b9c6a878f5
HieeusSama <vhieu31072004@gmail.com>
2023-12-09 12:27:34 +0700

    menu

--- main.cpp
+++ main.cpp
@@ -3,5 +3,9 @@
 int main()
 {
     account student;
+    title();
+    box(50, 3, 20, 2);
+    box(50, 6, 20, 2);
     student.login();
+    getch();
 }
--- support.h
+++ support.h
@@ -3,52 +3,57 @@
 #include <iostream>
 #include <fstream>
 #include <windows.h>
-
+#include <string>
+#include <conio.h>
 using namespace std;
 
+//Class
 class account
 {
     private:
         string username, password, user, pass;
+
     public:
+        //Ham kiem tra tai khoan
         bool check()
         {
-            cout<<"enter username: "; cin>> username;
-            cout<<"enter password: "; cin>> password;
+            cout << "enter username: "; cin >> username;
+            cout << "enter password: "; cin >> password;
 
             ifstream read(username + ".txt");
             getline(read, user);
             getline(read, pass);
             
-            if (user == username && pass == password)
+            if(user == username && pass == password)
             {
                 return true;
             }
             else
             {
                 return false;
-                
             }
         }
-
+        
+        //Ham dang nhap
         bool login()
         {
             int choice;
             try
             {
-                cout << "select a choice!\n1 : register\n2 : Login\n your choice: "; cin >> choice;
+                cout << endl;
+                cout << "select !\n1 : register\n2 : Login\n your choice: "; cin >> choice;
                 throw choice;
             }
             catch (...)
             {
-                cout<<"Vui long nhap lai !!!";
+                cout << "Vui long nhap lai !!!";
             }
             if (choice == 1)
             {
                 string username, password;
 
-                cout<<"select a username: "; cin>>username;
-                cout<<"select a password: "; cin>>password;
+                cout << "select a username: "; cin >> username;
+                cout << "select a password: "; cin >> password;
 
                 ofstream file;
                 file.open(username + ".txt");
@@ -62,13 +67,13 @@ class account
                 bool status = check();
                 if(!status)
                 {
-                    cout<<"incorrect information, try again!"<<endl;
+                    cout << "incorrect information, try again!" << endl;
                     system("pause");
                     return 0;
                 }
                 else
                 {
-                    cout<<"login successful!"<<endl;
+                    cout << "login successful!" << endl;
                     system("cls");
                     return 1;
                 }
@@ -82,15 +87,49 @@ class student
         int id;
         string address, classroom, majors;
     public:
-        
 };
 
-void gotoxy(int x, int y)
+//Function
+void gotoxy(short ix, short iy)
 {
-    static HANDLE h = NULL;  
-    if(!h)
-        h = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD c = { x, y };  
-    SetConsoleCursorPosition(h,c);
+  static HANDLE h = NULL;  
+  if(!h)
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD c = {ix, iy};
+  SetConsoleCursorPosition(h,c);
 }
+
+void box(int x, int y, int w, int h)
+{
+    if(h <= 1 || w <= 1)
+        return;
+    for(int i = x; i <= x + w; i++)
+    {
+        gotoxy(i, y);
+        cout << char(4);
+        gotoxy(i, y + h);
+        cout << char(4);
+    }
+    for(int i = y; i <= y + h; i++)
+    {
+        gotoxy(x, i);
+        cout << char(4);
+        gotoxy(x + w, i);
+        cout << char(4);
+    }
+}
+
+void title()
+{
+    string x = "QUAN LY SINH VIEN";
+    string y = "Login";
+    string z = "Register";
+    gotoxy(60 - x.length()/2,2);
+    cout << x;
+    gotoxy(60 - y.length()/2,4);
+    cout << y;
+    gotoxy(60 - z.length()/2,7);
+    cout << z;
+}
+
 #endif
